f7ef67567e0e121e5c1454bc376fc056f0d8dbca
Lucas Merritt <lucas.j.merritt@gmail.com>
2022-11-20 01:24:25 -0700

    added movestack patch

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static const char *dmenucmd[] = { "dmenurun", "-m", dmenumon, "-fn", dmenufont, 
 static const char *termcmd[]  = { "st", NULL };
 static const char *dmenucmd2[] = {"dmenu_run",NULL};
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,  spawn,          {.v = dmenucmd2 } },
@@ -76,6 +77,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_r,      reorganizetags, {0} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
