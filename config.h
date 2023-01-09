04eb095d0d085550ebfb41e2e35822cb338b2048
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-08 19:09:58 -0700

    changed colors

--- config.h
+++ config.h
@@ -13,17 +13,21 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_second[]      = "#602c94";
-static const char fg[]         = "#282828";
-static const char bg[]         = "#f2e5bc";
-static const char acc[]        = "#d79921";
+
+static const char fg[]          = "#dcd8c0";
+static const char bg[]          = "#b4af9a";
+static const char acc[]         = "#454138";
+static const char fg1[]         = "#282828";
+static const char bg1[]         = "#f2e5bc";
+static const char acc1[]        = "#d79921";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = {fg,bg,bg},
-	[SchemeSel]  = {bg,acc,acc},
+	[SchemeNorm] = {acc,bg,fg},
+	[SchemeSel]  = {fg,acc,acc},
 };
 
 /* tagging */
-static const char *tags[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i" };
+static const char *tags[] = { "a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8", "i9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
